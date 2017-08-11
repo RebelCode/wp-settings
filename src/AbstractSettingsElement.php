0dc7cf92aecd674f9423b93b63c9b2cd9c5a9633
Miguel Muscat <miguelmuscat93@gmail.com>
2017-08-11 17:37:55 +0200

    Added render context normalization

--- src/AbstractSettingsElement.php
+++ src/AbstractSettingsElement.php
@@ -32,4 +32,49 @@ abstract class AbstractSettingsElement extends AbstractValidatorBase
      * @since [*next-version*]
      */
     use LabelAwareTrait;
+
+    /**
+     * Normalizes the render context.
+     *
+     * @since [*next-version*]
+     *
+     * @param mixed $context The render context.
+     *
+     * @return string The normalized render context value to use for this instance.
+     */
+    protected function _normalizeRenderContext($context)
+    {
+        $key = $this->_getRenderContextKey();
+
+        if ($context instanceof ContainerInterface && $context->has($key)) {
+            return $context->get($key);
+        }
+
+        if ($context instanceof ValueAwareInterface) {
+            return $context->getValue();
+        }
+
+        if (is_array($context) || $context instanceof ArrayAccess) {
+            return $context[$key];
+        }
+
+        if ($context instanceof Stringable) {
+            return (string)$context;
+        }
+
+        return $context;
+    }
+
+    /**
+     * Retrieves the key to use when fetching a value from a composite render context.
+     *
+     * @since [*next-version*]
+     *
+     * @return string
+     */
+    protected function _getRenderContextKey()
+    {
+        return $this->_getKey();
+    }
+
 }
--- test/functional/AbstractSettingsElementTest.php
+++ test/functional/AbstractSettingsElementTest.php
@@ -2,6 +2,10 @@
 
 namespace RebelCode\WordPress\Admin\Settings\FuncTest;
 
+use ArrayObject;
+use DateTime;
+use Dhii\Data\Container\ContainerInterface;
+use Dhii\Data\ValueAwareInterface;
 use RebelCode\WordPress\Admin\Settings\AbstractSettingsElement;
 use Xpmock\TestCase;
 
@@ -13,7 +17,7 @@ use Xpmock\TestCase;
 class AbstractSettingsElementTest extends TestCase
 {
     /**
-     * The classname of the test subject.
+     * The class name of the test subject.
      *
      * @since [*next-version*]
      */
@@ -29,10 +33,64 @@ class AbstractSettingsElementTest extends TestCase
     public function createInstance()
     {
         $mock = $this->mock(static::TEST_SUBJECT_CLASSNAME)
-            ->_getValidationErrors()
-            ->new();
+                     ->_getValidationErrors()
+                     ->_createCouldNotRenderException()
+                     ->new();
 
         return $mock;
+    }
+
+    /**
+     * Creates a container mock for testing purposes.
+     *
+     * @since [*next-version*]
+     *
+     * @param mixed|null $get The value to retrieve when calling `get()`.
+     * @param bool       $has The return value of the `has()` method.
+     *
+     * @return ContainerInterface
+     */
+    public function createContainer($get = null, $has = true)
+    {
+        $mock = $this->mock('\Dhii\Data\Container\ContainerInterface')
+                     ->get($get)
+                     ->has($has);
+
+        return $mock->new();
+    }
+
+    /**
+     * Creates a value aware instance for testing purposes.
+     *
+     * @since [*next-version*]
+     *
+     * @param mixed|null $value The value that the instance should provide.
+     *
+     * @return ValueAwareInterface
+     */
+    public function createValueAware($value = null)
+    {
+        $mock = $this->mock('\Dhii\Data\ValueAwareInterface')
+                     ->getValue($value);
+
+        return $mock->new();
+    }
+
+    /**
+     * Creates a value aware instance for testing purposes.
+     *
+     * @since [*next-version*]
+     *
+     * @param string $string The string that the instance should be cast into.
+     *
+     * @return ValueAwareInterface
+     */
+    public function createStringable($string = '')
+    {
+        $mock = $this->mock('\Dhii\Util\String\StringableInterface')
+                     ->__toString($string);
+
+        return $mock->new();
     }
 
     /**
@@ -48,5 +106,147 @@ class AbstractSettingsElementTest extends TestCase
             static::TEST_SUBJECT_CLASSNAME, $subject,
             'Subject is not a valid instance'
         );
+    }
+
+    /**
+     * Tests the render context key getter method to ensure that it returns the element's key by default.
+     *
+     * @since [*next-version*]
+     */
+    public function testGetRenderContextKey()
+    {
+        $subject = $this->createInstance();
+        $reflect = $this->reflect($subject);
+
+        $reflect->_setKey($key = 'my-123-key');
+
+        $this->assertEquals($key, $reflect->_getRenderContextKey());
+    }
+
+    /**
+     * Tests the render context normalization method with a container containing the value to be used.
+     *
+     * @since [*next-version*]
+     */
+    public function testNormalizeRenderContextWithContainerHasValue()
+    {
+        $subject = $this->createInstance();
+        $reflect = $this->reflect($subject);
+
+        $key       = 'my-key';
+        $value     = 'my-value';
+        $container = $this->createContainer($value, true);
+        $reflect->_setKey($key);
+
+        $this->assertEquals($value, $reflect->_normalizeRenderContext($container));
+    }
+
+    /**
+     * Tests the render context normalization method with a container NOT containing the value to be used.
+     *
+     * @since [*next-version*]
+     */
+    public function testNormalizeRenderContextWithContainerNoValue()
+    {
+        $subject = $this->createInstance();
+        $reflect = $this->reflect($subject);
+
+        $key       = 'my-key';
+        $value     = null;
+        $container = $this->createContainer($value, false);
+
+        $this->assertEquals($container, $reflect->_normalizeRenderContext($container));
+    }
+
+    /**
+     * Tests the render context normalization method with a value aware instance.
+     *
+     * @since [*next-version*]
+     */
+    public function testNormalizeRenderContextWithValueAware()
+    {
+        $subject = $this->createInstance();
+        $reflect = $this->reflect($subject);
+
+        $valueAware = $this->createValueAware($value = 'my test value');
+
+        $this->assertEquals($value, $reflect->_normalizeRenderContext($valueAware));
+    }
+
+    /**
+     * Tests the render context normalization method with a stringable instance.
+     *
+     * @since [*next-version*]
+     */
+    public function testNormalizeRenderContextWithStringable()
+    {
+        $subject = $this->createInstance();
+        $reflect = $this->reflect($subject);
+
+        $stringable = $this->createStringable($string = 'my test string');
+
+        $this->assertEquals($string, $reflect->_normalizeRenderContext($stringable));
+    }
+
+    /**
+     * Tests the render context normalization method with an array.
+     *
+     * @since [*next-version*]
+     */
+    public function testNormalizeRenderContextWithArray()
+    {
+        $subject = $this->createInstance();
+        $reflect = $this->reflect($subject);
+
+        $array = [
+            'zero'  => 0,
+            'one'   => 1,
+            'two'   => 2,
+            'three' => 3,
+        ];
+        $reflect->_setKey('two');
+
+        $this->assertEquals(2, $reflect->_normalizeRenderContext($array));
+    }
+
+    /**
+     * Tests the render context normalization method with an object that supports array access.
+     *
+     * @since [*next-version*]
+     */
+    public function testNormalizeRenderContextWithArrayAccess()
+    {
+        $subject = $this->createInstance();
+        $reflect = $this->reflect($subject);
+
+        $arrayObject = new ArrayObject([
+            'zero'  => 0,
+            'one'   => 1,
+            'two'   => 2,
+            'three' => 3,
+        ]
+        );
+        $reflect->_setKey('two');
+
+        $this->assertEquals(2, $reflect->_normalizeRenderContext($arrayObject));
+    }
+
+    /**
+     * Tests the render context normalization method with a non-supported value.
+     *
+     * @since [*next-version*]
+     */
+    public function testNormalizeRenderContextWithNonSupportedValue()
+    {
+        $subject = $this->createInstance();
+        $reflect = $this->reflect($subject);
+
+        $number = 123456;
+        $string = 'test string';
+        $misc   = new DateTime();
+
+        $this->assertEquals($number, $reflect->_normalizeRenderContext($number));
+        $this->assertEquals($string, $reflect->_normalizeRenderContext($string));
+        $this->assertEquals($misc, $reflect->_normalizeRenderContext($misc));
     }
 }
